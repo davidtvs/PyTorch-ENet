027481cbc0979b633eb4781de9edca383413e2ef
David Silva <davidtvs10@gmail.com>
2018-05-29 23:28:26 +0100

    Improve argument parser

--- args.py
+++ args.py
@@ -13,20 +13,18 @@ def get_arguments():
         "-m",
         choices=['train', 'test', 'full'],
         default='train',
-        help="train: performs training and validation; test: tests the model "
-        "found in \"--save_dir\" with name \"--name\" on \"--dataset\"; "
-        "full: combines train and test modes. Default: train")
+        help=("train: performs training and validation; test: tests the model "
+              "found in \"--save_dir\" with name \"--name\" on \"--dataset\"; "
+              "full: combines train and test modes. Default: train"))
     parser.add_argument(
         "--resume",
-        type=bool,
-        default=False,
-        help="If set to true, the model found in \"--save_dir\" with name "
-        "\"--name\" is loaded and training is resumed from the epoch it was "
-        "saved in. Default: False")
+        action='store_true',
+        help=("The model found in \"--checkpoint_dir/--name/\" and filename "
+              "\"--name.h5\" is loaded."))
 
     # Hyperparameters
     parser.add_argument(
-        "--batch_size",
+        "--batch-size",
         "-b",
         type=int,
         default=10,
@@ -37,24 +35,24 @@ def get_arguments():
         default=300,
         help="Number of training epochs. Default: 300")
     parser.add_argument(
-        "--learning_rate",
+        "--learning-rate",
         "-lr",
         type=float,
         default=5e-4,
         help="The learning rate. Default: 5e-4")
     parser.add_argument(
-        "--lr_decay",
+        "--lr-decay",
         type=float,
         default=0.1,
         help="The learning rate decay factor. Default: 0.5")
     parser.add_argument(
-        "--lr_decay_epochs",
+        "--lr-decay-epochs",
         type=int,
         default=100,
         help="The number of epochs before adjusting the learning rate. "
         "Default: 100")
     parser.add_argument(
-        "--weight_decay",
+        "--weight-decay",
         "-wd",
         type=float,
         default=2e-4,
@@ -67,7 +65,7 @@ def get_arguments():
         default='camvid',
         help="Dataset to use. Default: camvid")
     parser.add_argument(
-        "--dataset_dir",
+        "--dataset-dir",
         type=str,
         default="data/CamVid",
         help="Path to the root directory of the selected dataset. "
@@ -88,6 +86,11 @@ def get_arguments():
         default='ENet',
         help="The class weighing technique to apply to the dataset. "
         "Default: enet")
+    parser.add_argument(
+        "--with-unlabeled",
+        dest='ignore_unlabeled',
+        action='store_false',
+        help="The unlabeled class is not ignored.")
 
     # Settings
     parser.add_argument(
@@ -96,21 +99,19 @@ def get_arguments():
         default=4,
         help="Number of subprocesses to use for data loading. Default: 4")
     parser.add_argument(
-        "--print_step",
-        type=bool,
-        default=False,
-        help="True to print step loss. Default: False")
+        "--print-step",
+        action='store_true',
+        help="Print loss every step")
     parser.add_argument(
-        "--imshow_batch",
-        type=bool,
-        default=False,
-        help="True displays batch images when loading dataset and predicting. "
-        "Default: False")
+        "--imshow-batch",
+        action='store_true',
+        help=("Displays batch images when loading the dataset and making "
+              "predictions."))
     parser.add_argument(
-        "--cuda",
-        type=bool,
-        default=True,
-        help="True to use CUDA (GPU). Default: True")
+        "--no-cuda",
+        dest='cuda',
+        default='store_false',
+        help="CPU only.")
 
     # Storage settings
     parser.add_argument(
@@ -119,31 +120,9 @@ def get_arguments():
         default='ENet',
         help="Name given to the model when saving. Default: ENet")
     parser.add_argument(
-        "--save_dir",
+        "--save-dir",
         type=str,
         default='save',
         help="The directory where models are saved. Default: save")
-
-    # Mutually exclusive groups
-    exclusive_group = parser.add_mutually_exclusive_group(required=False)
-    exclusive_group.add_argument(
-        "--unlabeled",
-        dest='ignore_unlabeled',
-        action='store_false',
-        help=(
-            "The unlabeled class is not ignored"
-        )
-    )
-    exclusive_group.add_argument(
-        "--no-unlabeled",
-        dest='ignore_unlabeled',
-        action='store_true',
-        help=(
-            "The unlabeled class is ignored"
-        )
-    )
-
-    # Set a default for so we don't have to always specify one or the other
-    parser.set_defaults(ignore_unlabeled=True)
 
     return parser.parse_args()
